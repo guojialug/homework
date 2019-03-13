08d342722cca20a1bf7553e720cec45cc368af7c
gjl <48385202+guojialug@users.noreply.github.com>
2019-03-13 16:51:22 +0800

    Update list.c

--- list.c
+++ list.c
@@ -30,3 +30,30 @@ void main()
     }
     printf("%d%d%d",X,Y,Z);
 }
+
+1.20
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define N 10
+int main()
+{
+	int i;
+	float x;
+	printf("input the x:\n");
+	scanf("%f",&x);
+	float a[N];
+	int n;
+	printf("please input the n:\n");
+	scanf("%d",&n);
+	printf("please input a[0]-a[n-1]:\n");
+	for(i=0;i<n;i++)
+	scanf("%f",&a[i]);
+	float result=a[n-1];
+	for(i=n-1;i>0;i--)
+	{
+		result=a[i-1]+x*result;
+	}
+	printf("the polynomail value is %f",result);
+	return 0;
+}
